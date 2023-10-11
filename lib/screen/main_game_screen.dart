ce3e961352db016a52b656e5797be5d93b965d97
Data <goliii_000@yahoo.com>
2023-10-11 19:01:08 +0330

    create ui

--- lib/screen/main_game_screen.dart
+++ lib/screen/main_game_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../util/calculate/move_object.dart';
+import '../util/object/object_type.dart';
+import '../util/object/random_object.dart';
+
 class MainGameScreen extends StatefulWidget {
   const MainGameScreen({Key? key}) : super(key: key);
 
@@ -7,9 +11,111 @@ class MainGameScreen extends StatefulWidget {
   State<MainGameScreen> createState() => _MainGameScreenState();
 }
 
-class _MainGameScreenState extends State<MainGameScreen> with TickerProviderStateMixin {
+class _MainGameScreenState extends State<MainGameScreen>
+    with TickerProviderStateMixin {
+  List<RandomObject> randomObj = [];
+
+  final int numOfObjects = 5;
+  final double objectSize = 80.0;
+  final double objectSpeed = 2.0;
+  AnimationController? controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initializeObjects();
+    startAnimating();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ScaleOps Test Task'),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Stack(
+          children: [
+            for (RandomObject obj in randomObj)
+              Positioned(
+                  left: obj.position.dx,
+                  top: obj.position.dy,
+                  //todo create custom container
+                  child: Container(
+                    width: obj.objectSize,
+                    height: obj.objectSize,
+                    color: obj.objectType == ObjectType.rock
+                        ? Colors.grey[400]
+                        : obj.objectType == ObjectType.paper
+                            ? Colors.lightBlue
+                            : Colors.green,
+                    child: Text(
+                      obj.objectType.toString().split('.').last,
+                      style: const TextStyle(
+                          fontSize: 24.0, fontWeight: FontWeight.bold),
+                    ),
+                  ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void initializeObjects() {
+    for (int i = 0; i < numOfObjects; i++) {
+      randomObj.add(
+        RandomObject(
+          objectSize: objectSize,
+          objectType: ObjectType.rock,
+          objectSpeed: objectSpeed,
+        ),
+      );
+    }
+
+    for (int i = 0; i < numOfObjects; i++) {
+      randomObj.add(
+        RandomObject(
+          objectSize: objectSize,
+          objectType: ObjectType.paper,
+          objectSpeed: objectSpeed,
+        ),
+      );
+    }
+
+    // Create 5 scissors
+    for (int i = 0; i < numOfObjects; i++) {
+      randomObj.add(
+        RandomObject(
+          objectSize: objectSize,
+          objectType: ObjectType.scissor,
+          objectSpeed: objectSpeed,
+        ),
+      );
+    }
+  }
+
+  void startAnimating() {
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(minutes: 1),
+      // duration: Duration(minutes:   1)
+    );
+
+    controller!.addListener(() {
+      setState(() {
+        moveObjects(context, randomObj);
+        // checkCollisions();
+      });
+    });
+    controller!.repeat();
   }
 }
